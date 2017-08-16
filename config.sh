891b3b4af9dc8c2c2c667442cbfd7e1833b1536e
Justin Lorieau <justin@lorieau.com>
2017-08-16 12:09:18 -0500

    Added file

---
+++ config.sh
@@ -0,0 +1,13 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    - pip install --only-binary=numpy,scipy numpy scipy pytest
+    - make install
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    pytest
+}
