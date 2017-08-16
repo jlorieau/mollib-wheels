f93b351078962fd9761570f9f1830b2df563af39
Justin Lorieau <justin@lorieau.com>
2017-08-16 15:08:36 -0500

    Use mollib absolute path

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ function pre_build {
 
 function run_tests {
     python --version
-    pytest mollib
+    pytest /io/mollib
 #    python -m pytest
 #    python -c 'import sys; import mollib; sys.exit(mollib.test())'
 #    ls /io/mollib/mollib
